16945a7823fa2a5230ba9801e3b3bc854a191979
neelk <neelesh.karthi@gmail.com>
2023-12-26 19:01:52 -0500

    updated code for notes app

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqlfb5d3ca1": {
       "AppClientID": "string",
